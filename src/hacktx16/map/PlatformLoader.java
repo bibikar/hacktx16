d17ff13185383eb21815df9df63d98e460d12305
Sameer Bibikar <bibikar@utexas.edu>
2016-10-22 18:25:46 -0500

    Create a platform loader
    Platforms will be stored in plain text files.

---
+++ src/hacktx16/map/PlatformLoader.java
@@ -0,0 +1,40 @@
+package hacktx16.map;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * 
+ * Platforms are specified in the following format, one on each line:
+ * x1 y1 x2 y2
+ * All values are ints.
+ *
+ */
+public class PlatformLoader {
+
+	private static String platformTest = "80 0 80 80\n80 90 80 120";
+	
+	public static Set<Platform> loadPlatforms(String platformInput) {
+		String[] split = platformInput.split("\n");
+		Set<Platform> platforms = new HashSet<>();
+		for (String s : split) 
+			platforms.add(loadPlatform(s));
+		return platforms;
+	}
+	
+	public static Set<Platform> loadTestPlatforms() {
+		return loadPlatforms(platformTest);
+	}
+	
+	public static Platform loadPlatform(String platformInput) {
+		Scanner scan = new Scanner(platformInput);
+		int x1 = scan.nextInt();
+		int y1 = scan.nextInt();
+		int x2 = scan.nextInt();
+		int y2 = scan.nextInt();
+		scan.close();
+		return new Platform(x1, y1, x2, y2);
+	}
+	
+}
