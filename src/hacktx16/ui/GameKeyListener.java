78d84daffd49add980790cc83ec1f5fbc16d8d09
CaptainL53 <kevinyesheng@gmail.com>
2016-10-22 16:24:25 -0500

    Press buttons.

--- src/hacktx16/ui/GameKeyListener.java
+++ src/hacktx16/ui/GameKeyListener.java
@@ -1,5 +1,30 @@
 package hacktx16.ui;
 
-public class GameKeyListener {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public abstract class GameKeyListener extends JFrame implements KeyListener{
+	
+	JTextField x = new JTextField(80);
+	
+	GameKeyListener(){
+	x.addKeyListener(this);
+	setSize(500,500);
+	setVisible(true);
+	setLocationRelativeTo(null);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	
+	public void keyTyped(KeyEvent e){
+		
+	}
+	public void keyPressed(KeyEvent e){
+		
+	}
+	public void keyReleased(KeyEvent txt){
+		
+	}
 }
